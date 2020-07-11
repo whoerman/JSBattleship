9ab955a6fa7b4b274431a6f0f2aa09a1d7a696ca
Walter Hoerman <whoerman@gmail.com>
2020-07-11 14:14:40 -0400

    working on messaging

--- main.js
+++ main.js
@@ -37,12 +37,13 @@ initializeData();
 console.log(boardsdata)
 
 
+
+
 //board play
 for (player = 0; player < 2; player++) {
     for (row = 0; row < 10; row++) {
         for (column = 0; column < 10; column++) {
             document.getElementById(player + '-' + (row + 1) + '-' + (column + 1)).addEventListener('click', function () {
-                console.log(this.id);
 
                 if (activePlayer == (this.id).charAt(0)) {
                     //comment don't shoot your own ships
@@ -55,13 +56,15 @@ for (player = 0; player < 2; player++) {
                     //chamge the value of that square's corresponding number in the board array to 8
                     boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] = 8;
                     //comment that it is a miss
-                    document.getElementById('comment0').textContent = 'You missed!';
+                    document.getElementById('comment0').textContent = 'Player '+(activePlayer+1)+' missed!';
                     //add to the missed score
                     PlayerMisses[activePlayer] += 1;
                     document.getElementById('miss' + activePlayer).textContent = ' Miss: ' + PlayerMisses[activePlayer];
-                    //change players
+                    //change players           
+                    // document.getElementById('P'+(activePlayer+1)+"ships").textContent = 'Player '+(activePlayer+1)+ ' Ships';
                     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary to switch it back and forth
-                    console.log(activePlayer);
+                    // document.getElementById('P'+(activePlayer+1)+"ships").textContent = '(Player '+(activePlayer+1)+ ' Shooting)';
+
 
                     //if the square has a ship
                 } else if (boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] > 0 && boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] < 6) {
@@ -69,13 +72,14 @@ for (player = 0; player < 2; player++) {
                     //chamge the value of that square's corresponding number in the board array to 9
                     boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] = 9;
                     //comment that it is a hit
-                    document.getElementById('comment0').textContent = 'It is a hit!!';
+                    document.getElementById('comment0').textContent = 'Player '+(activePlayer+1)+ ' has a hit!!';
                     //add to the hit score
                     PlayerHits[activePlayer] += 1;
                     document.getElementById('hits' + activePlayer).textContent = ' Hits: ' + PlayerHits[activePlayer];
                     //change players
+                    // document.getElementById('P'+(activePlayer+1)+"ships").textContent = 'Player '+(activePlayer+1)+ ' Ships';
                     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary to switch it back and forth
-                    console.log(activePlayer);
+                    // document.getElementById('P'+(activePlayer+1)+"ships").textContent = '(Player '+(activePlayer+1)+ ' Shooting)';
 
                 } else {
                     //comment that you already shot there
