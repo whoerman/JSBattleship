ce6913b8a261886e384ba526329745a608a6ac12
Walter Hoerman <whoerman@gmail.com>
2020-07-10 20:53:13 -0400

    graphic click change

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 for (a=0; a<2; a++) {
     for (x=1; x<11; x++) {
         for (y=1; y<11; y++)  {
-            document.getElementById('#'+a+'-'+x+'-'+y).addEventListener('click', function() {
-                document.getElementById('current-1').src = '../JSBattleship/empty.jpg'
+            document.getElementById(a+'-'+x+'-'+y).addEventListener('click', function() {
+                console.log(this.id);
+                document.getElementById(this.id).src = '../JSBattleship/empty.jpg';
             })
         }
     }
