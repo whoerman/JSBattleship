2146c4af1525071af55fbafdb0cbcb263c67d2b3
Walter Hoerman <whoerman@gmail.com>
2020-07-11 12:43:34 -0400

    more data

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ var boardsdata;
 
 // boardsdata[0][0][2] player (0 or 1) - row (1 to 10 so 0 to 9) - column (1 to 10 so 0 to 9)
 function initializeData() {
-    PlayerHits = [0,0]
-    PlayerMisses = [0,0]
+    PlayerHits = [0, 0]
+    PlayerMisses = [0, 0]
     activePlayer = 0;
     boardsdata = [
         [
+            [0, 0, 0, 0, 0, 0, 4, 4, 4, 0],
+            [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [3, 3, 3, 0, 0, 0, 0, 5, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0, 0],
         ],
         [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -46,39 +46,42 @@ for (player = 0; player < 2; player++) {
 
                 if (activePlayer == (this.id).charAt(0)) {
                     //comment don't shoot your own ships
-                    document.getElementById('comment0').textContent = 'Don\'t shoot your own ships!'; 
+                    document.getElementById('comment0').textContent = 'Don\'t shoot your own ships!';
 
-
-                } else if (boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] === 0) {
                     //if the square is empty
+                } else if (boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] === 0) {
                     //change the square to the nothing graphic
                     document.getElementById(this.id).src = '../JSBattleship/empty.jpg';
                     //chamge the value of that square's corresponding number in the board array to 8
                     boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] = 8;
                     //comment that it is a miss
-                    document.getElementById('comment0').textContent = 'You missed!'; 
+                    document.getElementById('comment0').textContent = 'You missed!';
                     //add to the missed score
                     PlayerMisses[activePlayer] += 1;
-                    document.getElementById('miss'+activePlayer).textContent = ' Miss: '+ PlayerMisses[activePlayer]; 
+                    document.getElementById('miss' + activePlayer).textContent = ' Miss: ' + PlayerMisses[activePlayer];
                     //change players
-                    activePlayer === 0 ? activePlayer = 1 : activePlayer= 0;  //ternary to switch it back and forth
-                } else if (boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] > 0 && boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] < 6) {
+                    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary to switch it back and forth
+                    console.log(activePlayer);
+
                     //if the square has a ship
+                } else if (boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] > 0 && boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] < 6) {
                     document.getElementById(this.id).src = '../JSBattleship/blast.jpg';
                     //chamge the value of that square's corresponding number in the board array to 9
                     boardsdata[parseInt((this.id).charAt(0))][parseInt((this.id).charAt(2)) - 1][parseInt((this.id).charAt(4)) - 1] = 9;
                     //comment that it is a hit
-                    document.getElementById('comment0').textContent = 'It is a hit!!'; 
+                    document.getElementById('comment0').textContent = 'It is a hit!!';
                     //add to the hit score
-                    PlayerHitss[activePlayer] += 1;
-                    document.getElementById('hits'+activePlayer).textContent = ' Hits: '+ PlayerHits[activePlayer]; 
+                    PlayerHits[activePlayer] += 1;
+                    document.getElementById('hits' + activePlayer).textContent = ' Hits: ' + PlayerHits[activePlayer];
                     //change players
-                    activePlayer === 0 ? activePlayer = 1 : activePlayer= 0;  //ternary to switch it back and forth
+                    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary to switch it back and forth
+                    console.log(activePlayer);
+
                 } else {
                     //comment that you already shot there
-                    document.getElementById('comment0').textContent = 'You already shot there!'; 
+                    document.getElementById('comment0').textContent = 'You already shot there!';
                 }
-                
+
 
 
             })
