b3f58e4b0aa6ea22b3503790528266493123f07d
Walter Hoerman <whoerman@gmail.com>
2020-07-11 07:58:12 -0400

    setting the data arrays

--- main.js
+++ main.js
@@ -1,11 +1,58 @@
+// //board setup
+// var newSquare = document.createElement('img')
+// for (a = 0; a < 2; a++) {
+//     a === 0 ? currentBoxLocation = 'left-div' : currentBoxLocation = 'right-div';
+//     for (x = 1; x < 11; x++) {
+//         currentRowLocation = a + '-' + x
+//         var rowDiv = document.createElement("div");
+//         document.getElementById(currentBoxLocation).appendChild(rowDiv).setAttribute('id', currentRowLocation);
+//         for (y = 1; y < 11; y++) {
+//             currentElementLocation = a + '-' + x + '-' + y;
+//             var imageBox = document.createElement("img");
+//             document.getElementById(currentRowLocation).appendChild(imageBox).setAttribute('id', currentElementLocation).setAttribute('src', '../JSBattleship/waves.jpg');
+//             console.log(currentBoxLocation, currentRowLocation, currentElementLocation);
+//         }
+//     }
+// }
 
-for (a=0; a<2; a++) {
-    for (x=1; x<11; x++) {
-        for (y=1; y<11; y++)  {
-            document.getElementById(a+'-'+x+'-'+y).addEventListener('click', function() {
-                console.log(this.id);
-                document.getElementById(this.id).src = '../JSBattleship/empty.jpg';
-            })
+let player1Array = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+];
+
+let player2Array = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+];
+
+
+
+    //board play
+    for (a = 0; a < 2; a++) {
+        for (x = 1; x < 11; x++) {
+            for (y = 1; y < 11; y++) {
+                document.getElementById(a + '-' + x + '-' + y).addEventListener('click', function () {
+                    console.log(this.id);
+
+                    //if the square is empty
+                    document.getElementById(this.id).src = '../JSBattleship/empty.jpg';
+                })
+            }
         }
     }
-}
